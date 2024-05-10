46ab9a17c1ec3d3f3e01f8a913235b09b4f8354b
Vinita Punjabi <vipunjabi@microsoft.com>
2024-05-10 11:36:12 +0530

    first commit

---
+++ poc-walmart/src/main/java/com/clustering/pocwalmart/PocWalmartApplication.java
@@ -0,0 +1,14 @@
+package com.clustering.pocwalmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocWalmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocWalmartApplication.class, args);
+		System.out.println("Hello Workd");
+	}
+
+}
---
+++ poc-walmart/src/test/java/com/clustering/pocwalmart/PocWalmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clustering.pocwalmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocWalmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
